7c05b81cfd94742f37b366d780cac690164a0ecb
Peter Michael Bearse <peterbearse@college.harvard.edu>
2016-11-29 23:29:37 +0000

    added important pset7 files

---
+++ application.py
@@ -0,0 +1,162 @@
+import cs50
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import gettempdir
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.route("/")
+@login_required
+def index():
+    while i < stockNumber:
+        stockValue = lookup.helpers(newStock)
+        print(stockValue)
+        i += 1
+    WHERE [ stockValue = stockNumber ]
+        for i < stockNumber:
+            GROUP BY stockNumber
+    SELECT * FROM "purchases"
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    newStock = cs50.get_string()
+    stockValue = lookup.helpers(newStock)
+    if stockValue is None:
+        return render_template("apology.html")
+    elif stockValue is NOT in "users":
+        return render_template("apology.html")
+    shares = cs50.get_int()
+    db.execute("SELECT cash FROM users")
+    CREATE TABLE 'purchases' ('username' TEXT, 'price' INTEGER, 'time' TEXT)
+    if cash < price:
+        return render_template("apology.html")
+    
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions."""
+    CREATE TABLE 'allpurchases' ('user' TEXT, 'value' INTEGER, 'key' TEXT)
+    SELECT * FROM 'allpurchases'
+    return render_template("history.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+        # query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # ensure username exists and password is correct
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+            return apology("invalid username and/or password")
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("login"))
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    stock = cs50.get_string()
+    if request.method == "GET":
+        return render_template("quote.html")
+    elif request.method == "POST":
+        return render_template("quoted.html")
+    else:
+        return 1
+
+    
+    return apology("TODO")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
+    username = cs50.get_string()
+    if username is None:
+        return apology
+    firstPassword = cs50.get_string()
+    # personal touch - encourage password strength
+    if len(firstPassword) < 8:
+        print("Consider a password that is at least eight characters for higher security.")
+    elif isalpha(firstPassword) is True:
+        print("Consider a password that includes some numeric characters or symbols.")
+    secondPassword = cs50.get_string()
+    if secondPassword != firstPassword:
+        return apology
+    hash = pwd_context.encrypt(firstPassword)
+    INSERT INTO "users" (username, password) VALUES ("username", "hash") 
+    return render_template("register.html")
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock."""
+    sellStock = cs50.get_string()
+    if sellStock is NOT in "purchases":
+        return render_template("apology.html")
+    elif sellStock is None:
+        return render_template("apology.html")
+    SELECT 'sellStock' FROM 'purchases'
+    stockValue = lookup.helpers("sellStock")
+    if stockValue is None:
+        return render_template("apology.html")
+    INSERT INTO 'purchases' (user, cash) VALUES('username', 'stockValue')
+    return 1
